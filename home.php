75dd5c79f55cc51ecc70935000f355d6c23b731c
Pritom Paul <pritompaul1920@gmail.com>
2022-05-25 13:18:44 +0600

    Edited imaages

---
+++ home.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+   <section class="header">
+
+      <a href="home.php" class="logo"><img src="images/tittle.png" width="150" height="90" alt="TripIt"></a>
+
+      <nav class="navbar">
+         <a href="home.php">Home</a>
+         <a href="about.php">About</a>
+         <a href="package.php">Package</a>
+         <a href="book.php">Book</a>
+      </nav>
+
+      <div id="menu-btn" class="fas fa-bars"></div>
+
+   </section>
+
+   <!-- home starts here -->
+   <section class="home">
+
+      <div class="swiper home-slider">
+
+         <div class="swiper-wrapper">
+
+            <div class="swiper-slide slide" style="background:url(images/home-slide-1.jpg) no-repeat">
+               <div class="content">
+                  <span>Explore, Discover & Travel!</span>
+                  <h3>Travel around the world, feel your life!</h3>
+                  <a href="package.php" class="btn">Discover more</a>
+               </div>
+            </div>
+
+            <div class="swiper-slide slide" style="background:url(images/home-slide-2.jpg) no-repeat">
+               <div class="content">
+                  <span>Explore, Discover & Travel!</span>
+                  <h3>Discover the new places</h3>
+                  <a href="package.php" class="btn">discover more</a>
+               </div>
+            </div>
+
+            <div class="swiper-slide slide" style="background:url(images/home-slide-3.jpg) no-repeat">
+               <div class="content">
+                  <span>Explore, discover & travel!</span>
+                  <h3>Make your tour worthwhile</h3>
+                  <a href="package.php" class="btn">discover more</a>
+               </div>
+            </div>
+
+         </div>
+
+         <div class="swiper-button-next"></div>
+         <div class="swiper-button-prev"></div>
+
+      </div>
+
+   </section>
+
+   <!-- home ends here-->
+   <!-- -------------------- -->
+   <!-- services starts here -->
+
+   <section class="services">
+
+      <h1 class="heading-title"> Our Services </h1>
+
+      <div class="box-container">
+         <div class="box" style="background-color: darkgreen;">
+            <img src="images/icon-1.png" alt="">
+            <h3>Adventure</h3>
+         </div>
+
+         <div class="box" style="background-color: darkolivegreen;">
+            <img src="images/icon-2.png" alt="">
+            <h3>Tour guide</h3>
+         </div>
+
+         <div class="box" style="background-color: green;">
+            <img src="images/icon-3.png" alt="">
+            <h3>Tracking</h3>
+         </div>
+
+         <div class="box" style="background-color: darkseagreen;">
+            <img src="images/icon-4.png" alt="">
+            <h3>Camp Fire</h3>
+         </div>
+
+         <div class="box" style="background-color: indianred;">
+            <img src="images/icon-5.png" alt="">
+            <h3>Off Road</h3>
+         </div>
+
+         <div class="box" style="background-color: dimgray;">
+            <img src="images/icon-6.png" alt="">
+            <h3>Camping</h3>
+         </div>
+
+      </div>
+
+   </section>
+
+   <!-- service ends here -->
+
+   <!-- home about section starts  -->
+
+   <section class="home-about">
+
+      <div class="image">
+         <img src="images/about-img2.png" alt="Logo of TripIt!">
+      </div>
+
+      <div class="content">
+         <h3>About us</h3>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita et, recusandae nobis fugit modi quibusdam ea assumenda, nulla quisquam repellat rem aliquid sequi maxime sapiente autem ipsum? Nobis, provident voluptate?</p>
+         <a href="about.php" class="btn">Read more</a>
+      </div>
+
+   </section>
+
+   <!-- home about section ends -->
+
+   <!-- home packages section starts  -->
+
+   <section class="home-packages">
+
+      <h1 class="heading-title"> our packages </h1>
+
+      <div class="box-container">
+
+         <div class="box">
+            <div class="image">
+               <img src="images/img-1.jpg" alt="">
+            </div>
+            <div class="content">
+               <h3>Adventure & tour</h3>
+               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, sint!</p>
+               <a href="book.php" class="btn">book now</a>
+            </div>
+         </div>
+
+         <div class="box">
+            <div class="image">
+               <img src="images/img-2.jpg" alt="">
+            </div>
+            <div class="content">
+               <h3>Adventure & tour</h3>
+               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, sint!</p>
+               <a href="book.php" class="btn">book now</a>
+            </div>
+         </div>
+
+         <div class="box">
+            <div class="image">
+               <img src="images/img-3.jpg" alt="">
+            </div>
+            <div class="content">
+               <h3>Adventure & tour</h3>
+               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, sint!</p>
+               <a href="book.php" class="btn">book now</a>
+            </div>
+         </div>
+
+      </div>
+
+      <div class="load-more"> <a href="package.php" class="btn">load more</a> </div>
+
+   </section>
+
+   <!-- home packages section ends -->
+
+   <!-- offer section  -->
+
+   <section class="home-offer">
+      <div class="content">
+         <h3>Upto 50% off for you lucky traveler!</h3>
+         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure tempora assumenda, debitis aliquid nesciunt maiores quas! Magni cumque quaerat saepe!</p>
+         <a href="book.php" class="btn">Book Now</a>
+      </div>
+   </section>
+
+   <!-- offer section-->
+
+   <!-- footer section starts  -->
+
+   <section class="footer">
+
+      <div class="box-container">
+
+         <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+            <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+            <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+            <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+         </div>
+
+         <div class="box">
+            <h3>extra links</h3>
+            <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+         </div>
+
+         <div class="box">
+            <h3>contact info</h3>
+            <a href="tel:+8801718382009"> <i class="fas fa-phone"></i> +8801718382009 </a>
+            <a href="tel:+8801733735071"> <i class="fas fa-phone"></i> +8801733735071 </a>
+            <a href="mailto:pritompaul1920@gmail.com?subject = Feedback&body = Message"> <i class="fas fa-envelope"></i> pritompaul19@gmail.com</a>
+            <a href="https://goo.gl/maps/C9t9o5vAL7unMEpa9"> <i class="fas fa-map"></i> Sylhet, Bangladesh </a>>
+         </div>
+
+         <div class="box">
+            <h3>follow us</h3>
+            <a href="https://www.facebook.com/pritompaul.pappu"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+         </div>
+
+      </div>
+
+      <div class="credit"> created by <span>Akhi Sarker & Pritom Paul</span> | all rights reserved! </div>
+
+   </section>
+
+   <!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+   <!-- swiper js link  -->
+   <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+   <!-- custom js file link  -->
+   <script src="js/script.js"></script>
+
+</body>
+
+</html>
